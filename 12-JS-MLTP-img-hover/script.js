0e29d43fae3cda7aa7b66024c9917cc68bb40cca
Sujoy Saha <ace.sujoy@gmail.com>
2025-11-16 21:29:14 +0530

    Done

--- 12-JS-MLTP-img-hover/script.js
+++ 12-JS-MLTP-img-hover/script.js
@@ -1,16 +1,18 @@
 let boxes = document.querySelectorAll('.imageContainer');
 
 boxes.forEach(box =>{
-    let img = box.querySelector('img');
+    let img =box.childNodes[3];
+
+    box.addEventListener('mouseenter',()=>{
+        img.style.opacity= 1;
+    });
+    box.addEventListener('mouseleave',()=>{
+        img.style.opacity= 0;
+    });
 
     box.addEventListener('mousemove',(event)=>{
         img.style.left= event.offsetX+'px';
         img.style.top= event.offsetY+'px';
-        img.style.display= 'initial';
-
-        setTimeout(() => {
-            img.style.display="none";
-        }, 1000);
     })
 });
 
